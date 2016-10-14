8fbc6c89b11a8a5a538ef9f3538fe34770feffbb
acabey <17acabey@gmail.com>
2016-10-14 15:40:26 +0000

    File paths to FHS. Fixed C9 runner

--- README.md
+++ README.md
@@ -8,11 +8,23 @@ Running X11 in a Cloud9 workspace.
 Installation
 ------------
 
+    Run the install script with privileges
+
     sudo ./install.sh
 
 Running
 -------
 
-    ./run.sh
+Use the custom C9 runner
     
-or use the `X11` runner.
+    Run > Run With > C9vnc
+    
+Or run the script directly from the /opt/ directory
+    
+    /opt/c9vnc/c9vnc.sh
+    
+You can also export an alias to this script and run with the alias
+    
+    alias c9vnc=/opt/c9vnc/c9vnc.sh
+    
+    c9vnc
--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+#Request sudo permissions
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
     echo "sudo $0 $*"
     exit 1
 fi
 
-
+#Get prerequisites
 sudo apt-get install supervisor xvfb fluxbox x11vnc
 
+#Copy supervisord configuration to proper configuration directory
 cp supervisord.conf ${HOME}/.config/supervisord.conf
 
+#Create the proper directory for the script
 sudo mkdir /opt/c9vnc
+
+#Make sure that the runners folder exists
 mkdir ${HOME}/workspace/.c9/runners
 
+#Copy the C9 runner to the C9 watch folder
 cp c9vnc.run ${HOME}/workspace/.c9/runners/c9vnc.run
+
+#Copy the run script to proper /opt/ directory
 cp run.sh /opt/c9vnc/run.sh
 
+#Clone noVNC into proper /opt/ directory
 git clone git://github.com/kanaka/noVNC /opt/noVNC/
-
