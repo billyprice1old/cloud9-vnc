cba0fb0d2ebf81024f02d451ca15471b46b941c8
acabey <17acabey@gmail.com>
2016-10-14 08:33:23 +0000

    Removed log. Note install sudo request

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd /home/ubuntu
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+cd ~/
 
 sudo apt-get install supervisor xvfb fluxbox x11vnc
 git clone git://github.com/kanaka/noVNC
